ac9e9794f317936ace5abf5431b6b44d2959b639
David Eglin <hello@djegl.in>
2022-12-02 16:29:19 +0000

    add day 2

---
+++ src/day02/main.py
@@ -0,0 +1,80 @@
+import os
+
+class RockPaperScissors:
+
+	def __init__(self, input: list):
+		self._rounds = [line.strip().split(' ') for line in input]
+
+	@classmethod
+	def read_file(cls):
+		absolute_path = os.path.dirname(os.path.abspath(__file__))
+		filename = absolute_path + "/input.txt"
+		with open(filename) as f:
+			return cls(f.readlines())
+
+	@classmethod
+	def calculate_simple_round_score(self, round):
+		move_map = {
+			'X': 1,
+			'Y': 2,
+			'Z': 3
+		}
+		winning_combos = ['CX', 'AY', 'BZ']
+		drawing_combos = ['AX', 'BY', 'CZ']
+		move = ''.join(round)
+		move_score = move_map.get(round[1])
+		result_score = 0
+		if move in winning_combos:
+			result_score = 6
+		elif move in drawing_combos:
+			result_score = 3
+		return move_score + result_score
+	
+	@classmethod
+	def calculate_complex_round_score(self, round):
+		shape_map = {
+			'A': 1,
+			'B': 2,
+			'C': 3
+		}
+		result_map = {
+			'X': 0,
+			'Y': 3,
+			'Z': 6
+		}
+		response_map = {
+			'A': {
+				'X': 'C',
+				'Y': 'A',
+				'Z': 'B'
+			},
+			'B': {
+				'X': 'A',
+				'Y': 'B',
+				'Z': 'C'
+			},
+			'C': {
+				'X': 'B',
+				'Y': 'C',
+				'Z': 'A'
+			}
+		}
+		return result_map.get(round[1]) + shape_map.get(response_map.get(round[0]).get(round[1]))
+
+	@property
+	def p1(self):
+		return sum([self.calculate_simple_round_score(round) for round in self._rounds])
+
+	@property
+	def p2(self):
+		return sum([self.calculate_complex_round_score(round) for round in self._rounds])
+
+
+def main():
+		rps = RockPaperScissors.read_file()
+		print('PART 1: Total score if following strategy guide:', rps.p1)
+		print('PART 2: Total score if following complex strategy guide:', rps.p2)
+
+if __name__ == '__main__':
+    import timeit
+    print(timeit.timeit(main, number=1))
